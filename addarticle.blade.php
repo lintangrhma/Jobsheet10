83dff62cf0e33a7285cb167ee2c346e1e187069e
lintangrhma <73377471+lintangrhma@users.noreply.github.com>
2020-11-14 18:35:10 +0700

    Add files via upload

---
+++ addarticle.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+ <label for="image">Feature Image</label>
+ <input type="file" class="form-control" required="required"
+name="image"></br>
+</div>
+<form action="/article/create" method="post"
+enctype="multipart/form-data">
